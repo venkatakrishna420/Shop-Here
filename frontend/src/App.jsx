4e04eaae26fcaf6b062c9b26d6b3269134b42e7a
venkata krishna <kakarlavenkatakrishna9@gmail.com>
2026-05-03 22:40:07 +0530

    feat: initialize frontend routing with App component and BrowserRouter configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,17 @@
 import './App.css'
 import LoginPage from './pages/LoginPage'
 import SingUpFrom from './pages/SignUpForm'
-
+import {Routes,Route} from 'react-router-dom'
 function App() {
 
   return (
     <>
-
-
-  {/* <SingUpFrom/> */}
-<LoginPage/>
-
+      <Routes>
+        <Route path="/" element={<SingUpFrom />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </>
-  )
+  ) 
 }
 
 export default App
